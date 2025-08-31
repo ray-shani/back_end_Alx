5ed8ae8ee29a795719acf0cd531b8aae67c51a45
Ray Shani <raysainepushani@gmail.com>
2025-08-31 21:08:17 +0300

    update: restarting the alx_hub_access app

--- ALXRegister/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "status", "created_at")
-    list_filter = ("status",)
-    search_fields = ("name", "email")
