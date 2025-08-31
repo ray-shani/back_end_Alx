4eb889e48c48159b376fdedc92ba7ab20ab157f3
Ray Shani <raysainepushani@gmail.com>
2025-08-31 21:13:13 +0300

    update:adding api's

---
+++ alx_hub_access/users/__pycache__/views.py
@@ -0,0 +1,12 @@
+# users/views.py
+from rest_framework import generics
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+
+class UserProfileListCreateView(generics.ListCreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class UserProfileRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
