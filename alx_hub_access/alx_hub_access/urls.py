079741609fd257cc5cc72ab78db195a7ad102c22
Ray Shani <raysainepushani@gmail.com>
2025-08-31 21:23:54 +0300

    update:making the api browsable friendly

--- alx_hub_access/alx_hub_access/urls.py
+++ alx_hub_access/alx_hub_access/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.http import api_root
 
-def home(request):
-    return HttpResponse("Welcome to ALX Hub Access API. Go to /api/users/ to manage users.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('', home),  # Root URL
+    path('', api_root),  
 ]
